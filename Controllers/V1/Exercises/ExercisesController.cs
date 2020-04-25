26f5150f198939eae73268573687d243fb187c56
MartinsPlume <56507599+MartinsPlume@users.noreply.github.com>
2020-04-25 20:58:50 +0300

    Added ExerciseController methods

--- Controllers/V1/Exercises/ExercisesController.cs
+++ Controllers/V1/Exercises/ExercisesController.cs
@@ -1,51 +1,124 @@
-﻿using System;
+﻿using EDzController.Data;
+using EDzController.Domain.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 
-namespace EDzController.Controllers.V1
+namespace EDzController.Controllers.V1.Exercises
 {
     [ApiController]
     [Route("api/[controller]")]
     public class ExercisesController : ControllerBase
     {
-        public ExercisesController()
+        private readonly AppDbContext _context;
+        public ExercisesController(AppDbContext context)
         {
-
+            _context = context;
         }
 
-        // GET: api/Exercise
         [HttpGet]
-        public IEnumerable<string> Get()
+        [Authorize(Roles = "Teacher")]
+        public IEnumerable<Exercise> GetProducts()
         {
-            return new string[] { "value1", "value2" };
+            return _context.Exercises;
         }
 
         // GET: api/Exercise/5
         [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
+        public async Task<IActionResult> GetExercise([FromRoute] int id)
         {
-            return "value";
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var exercise = await _context.Exercises.FindAsync(id);
+
+            if (exercise == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(exercise);
         }
 
         // POST: api/Exercise
         [HttpPost]
-        public void Post([FromBody] string value)
+        [Authorize(Roles = "Teacher")]
+        public async Task<IActionResult> PostProduct([FromBody] Exercise exercise)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Exercises.Add(exercise);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetExercise", new { id = exercise.ExerciseId }, exercise);
         }
 
         // PUT: api/Exercise/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        [Authorize(Roles = "Teacher")]
+        public async Task<IActionResult> PutExercise([FromRoute] int id, [FromBody] Exercise exercise)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != exercise.ExerciseId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(exercise).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ExerciseExists(id))
+                {
+                    return NotFound();
+                }
+
+                throw;
+            }
+
+            return NoContent();
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        [Authorize(Roles = "Teacher")]
+        public async Task<IActionResult> DeleteExercise([FromRoute] int id)
         {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var excercise = await _context.Exercises.FindAsync(id);
+            if (excercise == null)
+            {
+                return NotFound();
+            }
+
+            _context.Exercises.Remove(excercise);
+            await _context.SaveChangesAsync();
+            return Ok(excercise);
+        }
+
+        private bool ExerciseExists(int id)
+        {
+            return _context.Exercises.Any(e => e.ExerciseId == id);
         }
     }
 }
